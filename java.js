d898230ec2a34ea65160e5953702de03e0ebfa28
Kritib Bhattarai <bhatkr01@kritibs-mbp.lc.luther.edu>
2020-12-17 19:55:09 -0600

    initial commit

---
+++ java.js
@@ -0,0 +1,14 @@
+
+window.onload=function(){
+	const navToggle=document.querySelector('.nav_toggle');
+	const navLinks=document.querySelectorAll('.nav_link')
+	navToggle.addEventListener('click', ()=> {
+		document.body.classList.toggle('nav_open');
+	});
+
+	navLinks.forEach(link =>{
+		link.addEventListener('click', () =>{
+			document.body.classList.remove('nav_open');
+		})
+	})
+}
